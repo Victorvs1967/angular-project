8060a5955c74caa0d9a8dc169de001af2d0038a3
Victorvs1967 <victorsmirnov@me.com>
2023-02-24 12:03:01 +0200

    2222

--- src/app/modules/home/pages/home/home.component.ts
+++ src/app/modules/home/pages/home/home.component.ts
@@ -7,4 +7,24 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
 
+  color: string = '#000';
+  textColor = { 'color': this.color };
+
+  btnStatus: string = 'Button Not Clicked...'
+
+  user: any = {
+    name: 'Victor',
+    job: 'Technolog',
+  };
+
+  constructor() {
+    setTimeout(() => {
+      this.color =  'red';
+      this.textColor = { 'color': this.color };
+    }, 2000);
+  }
+
+  setBtnStatus() {
+    this.btnStatus = 'Button Clicked!'
+  }
 }
