128310f3cbabddbf6766ec9d89809287debd30d1
Victorvs1967 <victorsmirnov@me.com>
2023-03-04 22:42:56 +0200

    edit decorator

--- src/app/modules/auth/pages/user-dialog.decorator.ts
+++ src/app/modules/auth/pages/user-dialog.decorator.ts
@@ -1,7 +1,9 @@
 import { ComponentType } from "@angular/cdk/portal";
 import { DialogService } from "src/app/service/dialog.service";
+import { LoginComponent } from "./login/login.component";
+import { SignupComponent } from "./signup/signup.component";
 
-export const userModal = (componentType: ComponentType<any>) => {
+export const userModal = (componentType: ComponentType<LoginComponent | SignupComponent>) => {
   return (target: Object, propertyKey: string, descriptor: PropertyDescriptor, modalData: any = {},) => {
     const originalMethod = descriptor.value;
     descriptor.value = async function ( ...args: any ) {
